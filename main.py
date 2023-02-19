71ecd6bf5da9ac0367a7d25294cca76bc97484c7
evelinaKrigere <124894388+evelinaKrigere@users.noreply.github.com>
2023-02-19 18:10:57 +0000

    -

--- main.py
+++ main.py
@@ -14,16 +14,34 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next,i+1))
+            
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
+                return i+1
+            opening_brackets_stack.pop()
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1].position
+    return "Success"
+
+            
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
+    
+    text = input().strip()
+    if text[0]=='I':
+        text1=text[1:]
+    else:
+        text1=text
+
+    mismatch = find_mismatch(text1)
+    if mismatch=="Success":
+        print(mismatch)
+    else:
+        print(mismatch)
     # Printing answer, write your code here
 
 
